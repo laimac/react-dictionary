6b4e3670438867c356d12d71c2f01e8bd767e5c7
laimac <55thmouse@gmail.com>
2022-10-08 12:20:01 -0700

    added event behaviour

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Dictionary.css';
 
 export default function Dictionary(){
-function search(){
-alert("Searching");}
+    let [keyword, setKeyword]=useState(null);
+
+function handleKeywordChange(event){
+    setKeyword(event.target.value);
+}
+
+function search(event){
+    event.preventDefault();
+alert(`Searching for ${keyword} definition`);}
 
 return(
     <div className="dictionary">
 <form onSubmit={search}>
-<input type="search" placeholder="Search for a word..." autoFOcus={true}/>
+<input type="search" onChange={handleKeywordChange} placeholder="Search for a word..." autoFOcus={true}/>
 </form>
     </div>
 )

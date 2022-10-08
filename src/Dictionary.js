efd5d8308ab93d200eaa36f54d2cf5019ad08da3
laimac <55thmouse@gmail.com>
2022-10-08 14:10:06 -0700

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,33 @@
 import React, {useState} from 'react';
 import './Dictionary.css';
 import axios from "axios";
+import Results from './Results.js';
 
 export default function Dictionary(){
     let [keyword, setKeyword]=useState(null);
+    let [results, setResults]=useState(null);
+
+function handleResponse(response){
+setResults(response.data[0]);     
+}
+
+function search(event){
+    event.preventDefault();
+
+let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+ axios.get(apiUrl).then(handleResponse);
+}
 
 function handleKeywordChange(event){
     setKeyword(event.target.value);
 }
 
-function handleResponse(response){
-console.log(response.data[0]);
-}
-
-function search(event){
-    event.preventDefault();
-alert(`Searching for ${keyword} definition`);}
-let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-axios.get(apiUrl).then(handleResponse);
-
 return(
     <div className="dictionary">
 <form onSubmit={search}>
-<input type="search" onChange={handleKeywordChange} placeholder="Search for a word..." autoFOcus={true}/>
+<input type="search" onChange={handleKeywordChange} placeholder="Search for a word..." autoFocus={true}/>
 </form>
+<Results results={results}/>
     </div>
-)
-   ;
+);
 }

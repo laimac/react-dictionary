d3e22ddfd13049805a100cbb80aef621dc6847de
laimac <55thmouse@gmail.com>
2022-10-08 11:14:08 -0700

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './dictionary-logo.PNG'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<img src={logo} className="dictionary-logo img-fluid" alt="logo"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
